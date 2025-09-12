1354d457b2b710f75adaffeda015ebe624b19347
Jolanta Gryko <jgryko@gmail.com>
2025-09-12 09:49:12 +0200

    12.09

---
+++ bazy/01_podstawy/04_osoby.sql
@@ -0,0 +1,42 @@
+-- 1. Tworzysz bazę danych osób, które chcą nawiązać ze sobą kontakt. Dane: imię, nazwisko, zainteresowania. Utwórz bazę i tabele.
+
+mysql -u root
+create database 4e_2_Kontakty;
+show databases;
+use 4e_2_Kontakty;
+
+CREATE TABLE kontakty(
+    id int primary key auto_increment,
+    imie varchar(255),
+    nazwisko varchar(255),
+    zainteresowania varchar(255)
+);
+
+desc kontakty
+
+
+-- 2. Dodaj dane trzech osób: pierwsza ma dwa zainteresowania (bieganie, granie), druga jedno (serfowanie), trzecia cztery (granie, zwiedzanie, bieganie, łowienie ryb)
+
+insert into kontakty
+(imie, nazwisko,zainteresowania)
+values
+('Jan','Kowalski','bieganie, granie'),
+('Maciej','Malinowski','OTOMOTO'),
+('Filip','Beger','granie, zwiedzanie, bieganie, łowienie ryb');
+
+
+
+
+
+
+-- 3. Wyświetl wszystkie osoby które interesują się serfowaniem
+
+SELECT * 
+FROM kontakty 
+WHERE zainteresowania='OTOMOTO'; 
+-- 4. Wyświetl wszystkie osoby, które interesują się graniem
+SELECT *
+FROM kontakty
+WHERE zainteresowania='granie';
+
+-- 5. Jeśli trzeba - popraw strukturę tabel w bazie tak, aby powyższe zapytania były łatwe do napisania
