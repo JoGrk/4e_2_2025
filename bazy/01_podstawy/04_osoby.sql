f85c0e67894a9fbe01792b3d3923dc78af43d68b
Jolanta Gryko <jgryko@gmail.com>
2025-09-18 08:52:49 +0200

    18.09

--- bazy/01_podstawy/04_osoby.sql
+++ bazy/01_podstawy/04_osoby.sql
@@ -40,3 +40,63 @@ FROM kontakty
 WHERE zainteresowania='granie';
 
 -- 5. Jeśli trzeba - popraw strukturę tabel w bazie tak, aby powyższe zapytania były łatwe do napisania
+
+DROP TABLE kontakty;
+
+CREATE TABLE kontakty(
+    idkontaktu int primary key auto_increment,
+    imie varchar(255),
+    nazwisko varchar(255)
+);
+
+CREATE TABLE zainteresowania(
+    idZainteresowania int primary key auto_increment,
+    nazwa varchar(255)
+);
+
+CREATE TABLE Kontakty_zainteresowania(
+    idZainteresowania int,
+    idkontaktu int,
+    primary key (idZainteresowania,idkontaktu),
+    foreign key (idZainteresowania) references zainteresowania (idZainteresowania),
+    foreign key (idkontaktu) references Kontakty (idkontaktu)
+);
+
+insert into kontakty
+(imie, nazwisko)
+values
+('Jan','Kowalski'),
+('Maciej','Malinowski'),
+('Filip','Beger');
+
+insert into zainteresowania
+(nazwa)
+values
+('bieganie'),
+('OTOMOTO'),
+('granie'),
+('zwiedzanie'),
+('łowienie ryb');
+
+insert into Kontakty_zainteresowania
+(idkontaktu, idZainteresowania)
+values
+(1,1),
+(1,3),
+(2,2),
+(3,3),
+(3,4),
+(3,1),
+(3,5);
+
+
+('Jan','Kowalski','bieganie, granie'),
+('Maciej','Malinowski','OTOMOTO'),
+('Filip','Beger','granie, zwiedzanie, bieganie, łowienie ryb');
+
+SELECT imie, nazwisko, nazwa
+FROM kontakty 
+        JOIN Kontakty_zainteresowania ON kontakty.idkontaktu=Kontakty_zainteresowania.idkontaktu
+        JOIN zainteresowania ON zainteresowania.idZainteresowania=Kontakty_zainteresowania.idZainteresowania
+WHERE nazwa = 'bieganie';        
+
---
+++ bazy/01_podstawy/05_normalizacja.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Pracownicy(
+idPracownicy int primary key auto_increment,
+imie varchar(255),
+nazwisko varchar(255)
+);
+
+CREATE TABLE Jezyki(
+    idJezyki int primary key auto_increment,
+    
+)
