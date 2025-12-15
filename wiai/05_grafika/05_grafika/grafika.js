d03fd784964d73c13015be41c05c0c90598093a2
Jolanta Gryko <jgryko@gmail.com>
2025-12-15 15:34:25 +0100

    15.12

--- wiai/05_grafika/05_grafika/grafika.js
+++ wiai/05_grafika/05_grafika/grafika.js
@@ -2,6 +2,8 @@ const imgNavE = document.querySelectorAll('nav img')
 const leftBtnE = document.querySelector('#left')
 const rightBtnE = document.querySelector('#right')
 const imgMainE = document.querySelector('main img')
+const imgLeftE =document.querySelectorAll('.left img')
+const imgRightE =document.querySelectorAll('.right img')
 
 let imgIndex = 0
 
@@ -11,6 +13,7 @@ leftBtnE.addEventListener('click', e=>{
         imgIndex--
         imgMainE.src=imgNavE[imgIndex].src
     }
+    hiddenImg()
 })
 
 rightBtnE.addEventListener('click', e=>{
@@ -18,11 +21,33 @@ rightBtnE.addEventListener('click', e=>{
         imgIndex++
         imgMainE.src=imgNavE[imgIndex].src
     }
+    hiddenImg()
 })
 
 imgNavE.forEach((imgE, idx)=>{
     imgE.addEventListener('click',e=>{
         imgMainE.src=imgE.src
         imgIndex=idx
+        hiddenImg()
     })
 })
+
+function hiddenImg(){
+    imgLeftE.forEach((imgE, idx)=>{
+        if(idx>imgIndex){
+            imgE.style.display='inline-block'
+        }
+        else{
+            imgE.style.display='none'
+        }
+    })  
+    imgRightE.forEach((imgE, idx)=>{
+        if(idx<imgIndex){
+            imgE.style.display='inline-block'
+        }
+        else{
+            imgE.style.display='none'
+        }
+    })     
+}
+
