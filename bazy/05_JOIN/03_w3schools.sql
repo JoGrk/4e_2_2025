8de26af3a7c6cf878ed8b3d551cc5598b1dac162
Jolanta Gryko <jgryko@gmail.com>
2025-12-19 09:36:32 +0100

    19.12

--- bazy/05_JOIN/03_w3schools.sql
+++ bazy/05_JOIN/03_w3schools.sql
@@ -12,11 +12,25 @@ FROM orders
  FROM employees
     INNER JOIN orders ON employees.employeeID = orders.employeeID;
 -- 4. Wyświetl nazwy towarów, nazwy kategorii
- 
+SELECT ProductName, CategoryName
+FROM products
+    INNER JOIN categories ON Products.CategoryID = categories.CategoryID;
+
 -- 5. Wyświetl nazwy towarów, nazwy kategorii i idzamówień
+SELECT ProductName, CategoryName,OrderID
+FROM products
+    INNER JOIN categories ON Products.CategoryID = categories.CategoryID
+    INNER JOIN order_details ON products.ProductID = order_details.ProductID;
  
 -- 6. Wyświetl nazwy towarów, nazwy kategorii, daty zamówień i idklienta
  
+SELECT ProductName, CategoryName, OrderDate, CustomerID
+FROM products
+    INNER JOIN categories ON Products.CategoryID = categories.CategoryID
+    INNER JOIN order_details ON products.ProductID = order_details.ProductID
+    INNER JOIN Orders ON order_details.OrderID = orders.orderID;
+
+
 -- 7. Wyświetl nazwy towarów, nazwy kategorii, daty zamówień i nazwę firmy klienta
  
 -- 8. Ile razy był zamawiany dany produkt? (podaj nazwy produktów)
