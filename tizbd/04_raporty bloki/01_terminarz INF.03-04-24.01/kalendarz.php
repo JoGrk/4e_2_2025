142588dfbe060b03c248f3b68ca45596bc76df98
Jolanta Gryko <jgryko@gmail.com>
2026-01-12 13:39:40 +0100

    12.01

---
+++ tizbd/04_raporty bloki/01_terminarz INF.03-04-24.01/kalendarz.php
@@ -0,0 +1,43 @@
+<?php
+    // $link -> new mysqli('localhost', 'root', '', '');
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zadania na lipiec</title>
+    <link rel="stylesheet" href="./style.css"></link>
+</head>
+<body>
+    <header>
+        <div class="img">
+           <img src="./logo1.png" alt="lipiec"> 
+        </div>
+        
+        <div class="title">
+            <h1>Terminarz</h1>
+            <p>
+                najbliższe zadania:
+            </p>
+
+        </div>
+    </header>
+
+    <main>
+        <p></p>
+    </main>
+    <!-- <div class="main"></div> -->
+
+    <footer>
+        <a href="./sierpien.html">Terminarz na sierpień</a>
+        <p>Stronę wykonał:</p>
+    </footer>
+
+</body>
+</html>
+
+<?php
+    // $link -> close();
+?>
---
+++ tizbd/04_raporty bloki/01_terminarz INF.03-04-24.01/kwerendy.sql
@@ -0,0 +1,5 @@
+-- – Zapytanie 1: wybierające jedynie niepowtarzające się wartości z pola wpis dla zadań z pierwszych 7 dni lipca roku 2020 (od 1 do 7 lipca) oraz takich, gdzie pole wpis nie jest puste
+
+-- – Zapytanie 2: wybierające jedynie pola dataZadania i wpis dla zadań z miesiąca lipca
+
+-- – Zapytanie 3: wybierające jedynie pole miesiac i wpis dla wpisów zaczynających się na literę „S”
