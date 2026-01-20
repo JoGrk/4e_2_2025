c2bd57820a59ee425233b6cabb6dd61778570e6a
Jolanta Gryko <jgryko@gmail.com>
2026-01-20 09:38:14 +0100

    20.01

--- wiai/07_manipulacja/04_obrazki/obrazki.js
+++ wiai/07_manipulacja/04_obrazki/obrazki.js
@@ -1,5 +1,7 @@
 const imagesArray = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg', 'img5.jpg', 'img6.jpg',]
 const ulE = document.querySelector('ul')
+const randomE = document.querySelector('#random')
+const deleteE = document.getElementById('delete')
 
 imagesArray.forEach(element =>{
     let liE = document.createElement('li')
@@ -7,3 +9,28 @@ imagesArray.forEach(element =>{
     liE.innerHTML=`<a href="${element}" target="_blank">${element}</a>`
     ulE.appendChild(liE)
 })
+
+randomE.addEventListener('click', e=>{
+    let randomIndex = drawRandom(imagesArray.length)
+    // console.log(randomIndex)
+    let sectionE=document.createElement('section')
+    sectionE.classList.add('images')
+    let h3E=document.createElement('h3')
+    h3E.textContent=`${randomIndex}`
+    let imgE=document.createElement('img')
+    imgE.src=imagesArray[randomIndex]
+    sectionE.appendChild(h3E)
+    sectionE.appendChild(imgE)
+    document.querySelector('main').appendChild(sectionE)
+})
+
+function drawRandom(max){
+    let draw = Math.random()*max
+    return Math.floor(draw)
+}
+
+// if(docume.hasChildNodes()){
+//     deleteE.addEventListener('click', e=>{
+//         document.querySelector('main').removeChild(document.querySelector('main')[0])
+//     })
+// }
