935f930d5c0f175817c1cb479ab1c5736f2d91b6
Jolanta Gryko <jgryko@gmail.com>
2026-03-17 09:52:41 +0100

    17.03

--- wiai/08_czas/03_clear/1.js
+++ wiai/08_czas/03_clear/1.js
@@ -4,40 +4,47 @@ const h1E = document.querySelector('h1');
 
 let count = 10;
 
-const interval1 = setInterval(() => {
-    count--;
-    h1E.textContent = count;
-    if (count == 0) {
-        h1E.textContent = "BUM";
-        clearInterval(interval1);
+
+let countDown = setInterval(()=>{
+    h1E.textContent=count
+    count--
+    if(count==-1){
+        h1E.textContent = 'BUM'
+        clearInterval(countDown)
     }
-}, 1000);
+}, 1000)
+
 
 
 // 2. Po 10 sekundach jest wyświetlana na stronie sekcja główna
 
-const sectionE = document.querySelector('section');
+const sectionE = document.querySelector('main section');
 
-setTimeout(() => {
-    sectionE.style.display = 'block';
-}, 10000);
+setTimeout(()=>{
+    sectionE.style.display='block'
+},10000)
 
 
 // 3. W bloku bocznym wyświetlane są obrazy, do których ścieżki (nazwy) s,a zapisane  tablicy. Obrazki powinny być tworzone co 2 sekundy.
 
 const asideE = document.querySelector('aside');
 
-const images = ['2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg'];
+const images = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg', 'img5.jpg', 'img6.jpg'];
 
-let imgIndex = 0;
-
-const interval2 = setInterval(() => {
-    if (imgIndex < images.length) {
-        const imgE = document.createElement('img');
-        imgE.src = images[imgIndex];
-        asideE.appendChild(imgE);
-        imgIndex++;
-    } else {
-        clearInterval(interval2);
+// images.forEach(img =>{
+//     let imgE = document.createElement('img')
+//     imgE.src=img
+//     setInterval(()=>{
+//         asideE.appendChild(imgE)
+//     }, 1000)
+// })
+let index = 0
+const imgInterval =setInterval(()=>{
+    let imgE = document.createElement('img')
+    imgE.src=images[index]
+    asideE.appendChild(imgE)
+    index++
+    if (index>=images.length){
+        clearInterval(imgInterval)
     }
-}, 2000);
+},1000)
