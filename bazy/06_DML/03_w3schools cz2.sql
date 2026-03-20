c474e2d459170978d64e4fde31062a2718866ce4
Jolanta Gryko <jgryko@gmail.com>
2026-03-20 13:22:30 +0100

    20.03

--- bazy/06_DML/03_w3schools cz2.sql
+++ bazy/06_DML/03_w3schools cz2.sql
@@ -12,25 +12,79 @@ WHERE CustomerID IN (
 ); 
  
 -- 2. Usuń produkty o cenie mniejszej niż 15
- 
--- 3. Zmień numer dostawcy produktu o id 2 na dostawcę o id 3
+
+UPDATE order_details
+SET ProductID = null
+WHERE ProductID IN(
+    SELECT ProductID 
+    FROM Products 
+    WHERE Price < 15
+);
+DELETE FROM Products
+WHERE price < 15;
+
+-- 3. Zmień numer dostawcy produktu o id 2, na dostawcę o id 3
+
+UPDATE Products
+SET SupplierID = 3
+WHERE ProductID = 2;
  
 -- 4, Zmień osobę do kontaktów na siebie dla klienta o numerze 1
+UPDATE customers
+SET ContactName = 'roman'
+WHERE CustomerID = 1;
+
  
 -- 5. Zmniejsz o 5% cenę wszystkich produktów dostarczanych przez dostawcę o numerze 7
+ UPDATE Products 
+ SET Price = Price*0.95
+ WHERE SupplierID = 7;
  
 -- 6. Zwiększ cenę produktów z kategorii 1 o 10%
+
+UPDATE Products 
+SET price = price*1.10
+WHERE CategoryID = 1;
  
 -- 7. Dodaj nową kategorię produktów
+
+INSERT INTO Categories
+(CategoryName)
+VALUES
+('warzywa');
  
 -- 8. Z tabeli Pracownicy (Employees) usuń pracownika o numerze 3 (EmployeeID). 
 
+UPDATE orders
+SET EmployeeID = null
+WHERE EmployeeID = 3;
+DELETE FROM Employees
+WHERE EmployeeID = 3;
+
 -- 9. Dodaj jeszcze jednego dostawcę (tabela Shippers)
 
+INSERT INTO Shippers
+(ShipperName)
+VALUES
+('opel');
+
 -- 10. Podnieś cenę Tofu do 30 (ProductName -nazwa produktu, Price - cena, tabela Products)
+UPDATE Products
+SET price = 30
+WHERE ProductName= 'Tofu' AND price<30;
 
 -- 11. Podnieś o 10% cenę (Price) wszystkich produktów o nazwie (ProductName) zaczynającej się na literę C) (tabela Products)
-
+UPDATE Products 
+SET Price = price*1.10
+WHERE ProductName LIKE 'c%';
 -- 12. Usuń z tabeli OrderDetails zamówienia o ID 10443 oraz 10440 (OrderID).
 
+DELETE FROM Order_Details
+WHERE OrderID = 10443 OR OrderID = 10440;
+
 -- 13. Zmień adres (Address), miasto (City) i kraj (Country) dostawcy o numerze 2  (SupplierID) na ul. Pocztową, Nakło, Polskę. (tabela Suppliers)
+
+UPDATE Suppliers
+SET Address = 'ul. Pocztowa', City ='Nakło', Country ='Polska'
+WHERE SupplierID = 2;
+    
---
+++ wiai/08_czas/05_date/data2.js
@@ -0,0 +1,25 @@
+const pE = document.querySelector('.time')
+
+let goalDate = new Date()
+let CurrentDate = new Date()
+goalDate.setMinutes(21)
+goalDate.setSeconds(15)
+
+let DivDate = goalDate - CurrentDate
+
+// pE.textContent=`${goalDate.getMinutes()}:${goalDate.getSeconds()}`
+let m = Math.floor(DivDate / (1000*60))
+let s = Math.floor(DivDate / (1000)) - m*60
+pE.textContent = `${m}:${s}`
+
+let countdown = 
+setInterval(() => {
+    let CurrentDate = new Date()
+    let m = Math.floor(DivDate / (1000*60))
+    let s = Math.floor(DivDate / (1000)) - m*60
+    s = (s < 10)?`0${s}`:s
+    pE.textContent = `${m}:${s}`
+    if(m == 0 && s == 0){
+        clearInterval(countdown)
+    }
+}, 10);
