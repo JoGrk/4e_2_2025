645f229aaa1b6ac9fb9f3f375481805b542d379f
Jolanta Gryko <jgryko@gmail.com>
2026-04-01 13:20:56 +0200

    1.04

--- tizbd/05_formularze/06_dania/dodawanie.php
+++ tizbd/05_formularze/06_dania/dodawanie.php
@@ -1,6 +1,17 @@
 <?php
-$link = new mysqli ('localhost', 'root','','4e_2_baza')
-$sql =
+$link = new mysqli ('localhost', 'root','','4e_2_baza');
+
+$dish_name_f=$_POST['dish-name']??NULL;
+$price_f=$_POST['price']??NULL;
+$dish_type_f=$_POST['dish-type']??NULL;
+
+if($dish_name_f && $price_f && $dish_type_f){
+    $sql="INSERT INTO dania (typ,nazwa,cena)
+    VALUES
+    ($dish_type_f,'$dish_name_f',$price_f);
+    ";
+    $result = $link->query($sql);
+};
 ?>
 
 
@@ -12,6 +23,20 @@ $sql =
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+        if($dish_name_f && $price_f && $dish_type_f){
+            if($result){
+                echo"<p>Danie $dish_name_f o cenie $price_f zostało dodane do bazy</p>";
+            }
+        }
+        else{
+            echo"<p>Do formularza nie wpisano danych</p>";
+        }
+    ?>
+    <a href="index.php">powrót na strone główna</a>
 </body>
 </html>
+
+<?php
+$link -> close();
+?>
